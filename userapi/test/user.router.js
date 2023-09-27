a283660a52b11be4ba6373a2900fdd91bff554b6
slkjayy93 <jaishan23@hotmail>
2023-09-27 22:44:30 +0200

    feat 2.2.1

--- userapi/test/user.router.js
+++ userapi/test/user.router.js
@@ -21,9 +21,9 @@ describe('User REST API', () => {
 
     it('create a new user', (done) => {
       const user = {
-        username: 'sergkudinov',
-        firstname: 'Sergei',
-        lastname: 'Kudinov'
+        username: 'jaishan',
+        firstname: 'Jaishan',
+        lastname: 'BURTON'
       }
       chai.request(app)
         .post('/user')
@@ -41,8 +41,8 @@ describe('User REST API', () => {
     
     it('pass wrong parameters', (done) => {
       const user = {
-        firstname: 'Sergei',
-        lastname: 'Kudinov'
+        firstname: 'Jaishan',
+        lastname: 'BURTON'
       }
       chai.request(app)
         .post('/user')
@@ -59,7 +59,57 @@ describe('User REST API', () => {
     })
   })
 
-  // describe('GET /user', ()=> {
-  //   // TODO Create test for the get method
-  // })
+  describe('GET /user', () => {
+    it('successfully get user', (done) => {
+      // On crée d'abord un user pour faire nos tests
+      const newUser = {
+        username: 'jaishan',
+        firstname: 'Jaishan',
+        lastname: 'BURTON'
+      };
+      
+      // On ajoute l'user dans la database
+      chai.request(app)
+        .post('/user')
+        .send(newUser)
+        .then((res) => {
+          chai.expect(res).to.have.status(201);
+          
+          // On fait la requête GET pour récupérer l'user créé
+          chai.request(app)
+            .get('/user/jaishan')
+            .then((getResponse) => {
+              chai.expect(getResponse).to.have.status(200);
+              chai.expect(getResponse.body.status).to.equal('success');
+              chai.expect(getResponse).to.be.json;
+              // On vérifie si les données de l'utilisateur correspondent aux données créées
+              chai.expect(getResponse.body.data).to.deep.equal(newUser);
+              done()
+            })
+            .catch((err) => {
+              throw err
+            })
+        })
+        .catch((err) => {
+          throw err
+        })
+    })
+  
+    it('cannot get a user when it does not exist', (done) => {
+      // Essayez de récupérer un utilisateur qui n'existe pas
+      chai.request(app)
+        .get('/user/nonexistentuser')
+        .then((getResponse) => {
+          chai.expect(getResponse).to.have.status(404);
+          chai.expect(getResponse.body.status).to.equal('error');
+          chai.expect(getResponse).to.be.json;
+          // Assurez-vous que le message d'erreur indique que l'utilisateur n'existe pas
+          chai.expect(getResponse.body.message).to.equal("L'utilisateur n'existe pas");
+          done();
+        })
+        .catch((err) => {
+          throw err
+        })
+    })
+  })
 })
