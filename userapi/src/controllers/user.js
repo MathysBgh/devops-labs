d9dc4b0fe5743bf167c654088297945cfa869a72
slkjayy93 <jaishan23@hotmail>
2023-09-27 21:05:46 +0200

    feat 2.1 done

--- userapi/src/controllers/user.js
+++ userapi/src/controllers/user.js
@@ -5,19 +5,45 @@ module.exports = {
     // Check parameters
     if(!user.username)
       return callback(new Error("Wrong user parameters"), null)
-    // Create User schema
+    
+      // Create User schema
     const userObj = {
       firstname: user.firstname,
       lastname: user.lastname,
     }
-    // Save to DB
     // TODO check if user already exists
+
+    db.hexists(user.username, 'firstname', (err, exists) => {
+      if (err) {
+        return callback(err, null);
+      }
+
+      if (exists) {
+        return callback(new Error("L'utilisateur existe déjà"), null);
+      }
+    // Save to DB
+
     db.hmset(user.username, userObj, (err, res) => {
       if (err) return callback(err, null)
       callback(null, res) // Return callback
     })
+  })
   },
-  // get: (username, callback) => {
-  //   // TODO create this method
-  // }
+   get: (username, callback) => {
+     // TODO create this method
+     // Récupérer les données de l'utilisateur à partir de la database
+      db.hgetall(username, (err, userData) => {
+        if (err) {
+          return callback(err, null);
+        }
+  
+        if (!userData || Object.keys(userData).length === 0) {
+          // L'utilisateur n'existe pas
+          return callback(new Error("L'utilisateur n'existe pas"), null);
+        }
+  
+        // L'utilisateur a été trouvé, renvoyer ses données
+        callback(null, userData);
+      })
+    }
 }
