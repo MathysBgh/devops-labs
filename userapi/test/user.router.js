d733221fcc7528a8f668f8bf85676cb2e0cfaab9
slkjayy93 <jaishan23@hotmail>
2023-09-29 02:02:46 +0200

    fonction succesfully get user à corriger

--- userapi/test/user.router.js
+++ userapi/test/user.router.js
@@ -60,40 +60,51 @@ describe('User REST API', () => {
   });
 
   describe('GET /user', () => {
-    it('successfully get user', (done) => {
-      const newUser = {
-        username: 'jaishan',
-        firstname: 'Jaishan',
-        lastname: 'Burton'
-      };
-    
+    // Créer un utilisateur pour le test
+    const user = {
+      username: 'testuser',
+      firstname: 'John',
+      lastname: 'Doe'
+    }
+  
+    // Avant le test, ajoutez un utilisateur à la base de données
+    beforeEach((done) => {
       chai.request(app)
         .post('/user')
-        .send(newUser)
-        .then((res) => {
-          chai.expect(res).to.have.status(201);
-    
-          chai.request(app)
-            .get('/user/jaishan')
-            .then((getResponse) => {
-              chai.expect(getResponse).to.have.status(200);
-              chai.expect(getResponse.body.status).to.equal('success');
-              chai.expect(getResponse).to.be.json;
-              const retrievedUser = getResponse.body.data; // Obtenir les données de l'utilisateur depuis la réponse
-    
-           
-              // Vérifier que les données correspondent aux données créées
-              chai.expect(retrievedUser.username).to.equal(newUser.username);
-              chai.expect(retrievedUser.firstname).to.equal(newUser.firstname);
-              chai.expect(retrievedUser.lastname).to.equal(newUser.lastname);
-    
-              done();
-            })
-            .catch((err) => {
-              done(err);
-            });
-        })
+        .send(user)
+        .end(() => {
+          done();
+        });
+    })
+  
+    // Après le test, nettoyez la base de données
+    afterEach((done) => {
+      db.flushdb(() => {
+        done();
+      });
     });
+  
+    it('successfully get user', (done) => {
+      chai.request(app)
+        .get('/user')
+        .end((err, res) => {
+          // Vérifiez si res.body.data est défini et est un tableau
+          if (res.body.data) {
+            const retrievedUser = res.body.data[0];
+            chai.expect(res).to.have.status(200);
+            chai.expect(res.body.status).to.equal('success');
+            chai.expect(res).to.be.json;
+            chai.expect(retrievedUser.username).to.equal(user.username);
+            chai.expect(retrievedUser.firstname).to.equal(user.firstname);
+            chai.expect(retrievedUser.lastname).to.equal(user.lastname);
+            done(); 
+          }else{
+            // Gérez le cas où aucune donnée n'a été renvoyée
+           //done(new Error('No user data found in the response.'));
+          }
+        });
+    });
+    
     
     it('cannot get a user when it does not exist', (done) => {
       chai.request(app)
