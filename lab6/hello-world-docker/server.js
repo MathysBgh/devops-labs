46b9301e12722346098fb231200ea7acd77982cf
slkjayy93 <74787049+slkjayy93@users.noreply.github.com>
2023-10-17 20:08:44 +0200

    feat : share docker container with a classmate

--- lab6/hello-world-docker/server.js
+++ lab6/hello-world-docker/server.js
@@ -6,7 +6,7 @@ const PORT = 8080;
 
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World from Docker!');
+  res.send('Hello World from Jaishan!');
 });
 
 app.listen(PORT);
